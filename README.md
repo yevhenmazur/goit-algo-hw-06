76660ea1503cdb7fa5f548c6809c415ac15f4be6
Yevhen Mazur <yevhen.mazur@pm.me>
2024-07-27 18:38:00 +0200

    Task 1 - done

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # goit-algo-hw-06
 Homework: Graphs
-У якості об'єктного графу я згенерував граф залежностей systemd юнатів на своєму комп'ютері за допомогою команд systemd-analyze dot | dot -Tsvg > dependency_graph.svg і systemd-analyze dot > dependency_graph.dot
+У якості об'єктного графу я згенерував граф залежностей systemd юнітів на робочій ОС свого комп'ютера за допомогою команд systemd-analyze dot | dot -Tsvg > dependency_graph.svg і systemd-analyze dot > dependency_graph.dot
 
 dependency_graph.dot перетворюю у структуру данних python за допомогою networkx 
+
+Утворений граф містить 203 вузли і 1395 ребер
+Це складний для візуалізації граф, тому я написав функцію format_labels_for_viz(graph), яка видаляє неінформативні частини лейьлів, щоб вони не загромаджували візуалізацію.
--- main.py
+++ main.py
@@ -1,12 +1,50 @@
 import networkx as nx
+import matplotlib.pyplot as plt
+import re
 
-# Зчитування графу з файлу DOT
-graph = nx.drawing.nx_pydot.read_dot('dependency_graph.dot')
+def format_labels_for_viz(graph):
+    '''This function removes uninformative parts of graph labels to simplify visualization'''
+    formatted_labels = {}
+    remove_words = ["service", "target", "device", "swap", "socket", "mount"]
+    
+    for node in graph.nodes:
+        # Розділити лейбл вузла за роздільниками
+        label_parts = re.split(r'[\\|:|@]', node)
+        # Взяти останню частину після роздільника
+        cleaned_label = label_parts[-1]
+        # Видалити небажані слова
+        for word in remove_words:
+            cleaned_label = cleaned_label.replace(word, "")
+            cleaned_label = cleaned_label.rstrip('.')
+        # Видалити зайві пробіли
+        cleaned_label = cleaned_label.strip()
+        # Зберегти форматований лейбл
+        formatted_labels[node] = cleaned_label
+    
+    return formatted_labels
 
-# Конвертація у формат JSON
-data = nx.node_link_data(graph)
 
-# Запис у файл JSON
-import json
-with open('dependency_graph.json', 'w') as f:
-    json.dump(data, f, indent=4)
+# Зчитую граф з файлу DOT
+G = nx.drawing.nx_pydot.read_dot('dependency_graph.dot')
+
+plt.figure(figsize=(12, 12))
+pos = nx.kamada_kawai_layout(G) # <- дуже прогресивний лейаут
+labels = format_labels_for_viz(G)
+
+options = {
+    "node_color": "blue",
+    "edge_color": "gray",
+    "node_size": 100,
+    "labels": labels,
+    "alpha": 0.7,
+    "with_labels": True,
+    "font_size": 8,
+    "font_color": "black"
+}
+
+nx.draw(G, pos, **options)
+plt.show()
+
+# print(G.number_of_nodes())
+# print(G.number_of_edges())
+
